65b3100df00ae6a99bb9054d5ed5cd75cfea749a
Raffles <rafflesandrison@gmail.com>
2022-06-24 17:52:48 +0800

    Initialise repo 2

--- question_dashboard.py
+++ question_dashboard.py
@@ -3,91 +3,214 @@ import json
 data_breakdown = {
     "MCQ": {
         "Primary 1": {
-            "topics": [
-                {
-                    "topic": "Whole Numbers",
+            "topics": {
+                "Whole Numbers": {
                     "question_count": 0,
                     "difficulty_1": 0,
                     "difficulty_2": 0,
                     "difficulty_3": 0,
-                    "subtopics": [
-                        {
-                            "subtopic": "Numbers Up To 100",
+                    "subtopics": {
+                        "Numbers Up To 100": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         },
-                        {
-                            "subtopic": "Addition And Subtraction",
+                        "Addition And Subtraction": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         },
-                        {
-                            "subtopic": "Multiplication And Division",
+                        "Multiplication And Division": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         },
-                        {
-                            "subtopic": "Money",
+                        "Money": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         }
-                    ]
+                    }
                 },
-                {
-                    "topic": "Measurement",
+                "Measurement": {
                     "question_count": 0,
                     "difficulty_1": 0,
                     "difficulty_2": 0,
                     "difficulty_3": 0,
-                    "subtopics": [
-                        {
-                            "subtopic": "Length",
+                    "subtopics": {
+                        "Length": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         },
-                        {
-                            "subtopic": "Time",
+                        "Time": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         }
-                    ]
+                    }
                 },
-                {
-                    "topic": "Geometry",
+                "Geometry": {
                     "question_count": 0,
                     "difficulty_1": 0,
                     "difficulty_2": 0,
                     "difficulty_3": 0,
-                    "subtopics": [
-                        {
-                            "subtopic": "2D Shapes",
+                    "subtopics": {
+                        "2D Shapes": {
                             "question_count": 0,
                             "difficulty_1": 0,
                             "difficulty_2": 0,
                             "difficulty_3": 0
                         }
-                    ]
+                    }
+                },
+                "Data Representation And Interpretation": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Picture Graphs": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
                 }
-            ]
+            }
+        },
+        "Primary 2": {
+            "topics": {
+                "Whole Numbers": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Numbers Up To 1000": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "Addition And Subtraction": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "Multiplication And Division": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Fractions": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Fraction Of A Whole": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "Addition And Subtraction": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Money": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Money": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Measurement": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Length, Mass And Volume": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "Time": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Geometry": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "2D Shapes": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "3D Shapes": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Data Representation And Interpretation": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Picture Graphs With Scales": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
+p1_whole_numbers_easy_uuids = ""
+p1_whole_numbers_normal_uuids = ""
 
-
-with open('math.txt', 'r', encoding='utf-8') as math_questions:
+with open('math_new.txt', 'r', encoding='utf-8') as math_questions:
     questions = json.load(math_questions)["data"]["questions"]
     
     count = 0
@@ -101,12 +224,38 @@ with open('math.txt', 'r', encoding='utf-8') as math_questions:
 
         # << To be deleted
         print("UUID = " + uuid + " ;\tlevel = " + level + " ;\ttopic=" + topic + " ;\tsubtopic = " + subtopic + " ;\tdifficultyLevel = " + difficultyLevel)
-        print(count)
+        #print(count)
         count = count + 1
         # To be deleted >>
 
-        for data_topic in data_breakdown[question_type][level]:
-            if (data_topic["topic"] == topic):
-                print("SAME TOPIC")
+        #print(data_breakdown[question_type]["Primary 1"])
+
+        mcqs = data_breakdown[question_type]
+        # mcq_primary_1_topics = mcqs["Primary 1"]["topics"]
+        
+        # Incrementing count
+        if (level == "Primary 1" or level == "Primary 2"):
+            mcq_topics = mcqs[level]["topics"]
             
+            if len(topic) > 0 and topic in mcq_topics:
+                mcq_topics[topic]["question_count"] = mcq_topics[topic]["question_count"] + 1
+                mcq_topics[topic]["difficulty_1"] = mcq_topics[topic]["difficulty_1"] + 1 if difficultyLevel == "1" else mcq_topics[topic]["difficulty_1"]
+                mcq_topics[topic]["difficulty_2"] = mcq_topics[topic]["difficulty_2"] + 1 if difficultyLevel == "2" else mcq_topics[topic]["difficulty_2"]
+                mcq_topics[topic]["difficulty_3"] = mcq_topics[topic]["difficulty_3"] + 1 if difficultyLevel == "3" else mcq_topics[topic]["difficulty_3"]
+            
+            if len(subtopic) > 0 and topic in mcq_topics and subtopic in mcq_topics[topic]["subtopics"]:
+                print(topic)
+                print(subtopic)
+                mcq_topics[topic]["subtopics"][subtopic]["question_count"] = mcq_topics[topic]["subtopics"][subtopic]["question_count"] + 1
+                mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"] + 1 if difficultyLevel == "1" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"]
+                mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"] + 1 if difficultyLevel == "2" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"]
+                mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"] + 1 if difficultyLevel == "3" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"]
+        
+print(data_breakdown)
+
+# print("\nP1 Whole Numbers Easy")
+# print(p1_whole_numbers_easy_uuids)
+
+# print("\nP1 Whole Numbers Normal")
+# print(p1_whole_numbers_normal_uuids)
 
