d5c2abd163919573d7d1001c7974a69e98369dbe
Raffles <rafflesandrison@gmail.com>
2022-06-27 17:36:07 +0800

    Add requirements file for dependencies and added Primary 5 and 6 Math mapping

--- question_dashboard.py
+++ question_dashboard.py
@@ -645,9 +645,139 @@ data_breakdown = {
                     }
                 }
             }
+        },
+        "Primary 6": {
+            "topics": {
+                "Fractions": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Four Operations": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Percentage": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Percentage": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Ratio": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Ratio": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Rate And Speed": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Distance, Time And Speed": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Algebra": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Algebra": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Area And Volume": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Area And Circumference Of Circle": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "Volume Of Cube And Cuboid": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Geometry": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Special Quadrilaterals ": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        },
+                        "Nets": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                },
+                "Data Representation And Interpretation": {
+                    "question_count": 0,
+                    "difficulty_1": 0,
+                    "difficulty_2": 0,
+                    "difficulty_3": 0,
+                    "subtopics": {
+                        "Pie Charts": {
+                            "question_count": 0,
+                            "difficulty_1": 0,
+                            "difficulty_2": 0,
+                            "difficulty_3": 0
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+uuids_with_issues = []
 
 p1_whole_numbers_easy_uuids = ""
 p1_whole_numbers_normal_uuids = ""
@@ -664,41 +794,33 @@ with open('math_new.txt', 'r', encoding='utf-8') as math_questions:
         subtopic = question["subtopics"][0] if len(question["subtopics"]) > 0 else ""
         difficultyLevel = question["difficultyLevel"][0] if len(question["difficultyLevel"]) > 0 else ""
 
-        # << To be deleted
-        print("UUID = " + uuid + " ;\tlevel = " + level + " ;\ttopic=" + topic + " ;\tsubtopic = " + subtopic + " ;\tdifficultyLevel = " + difficultyLevel)
-        #print(count)
-        count = count + 1
-        # To be deleted >>
-
-        #print(data_breakdown[question_type]["Primary 1"])
-
         mcqs = data_breakdown[question_type]
-        # mcq_primary_1_topics = mcqs["Primary 1"]["topics"]
         
         # Incrementing count
-        if (level == "Primary 1" or level == "Primary 2"):
+        if (level == "Primary 1" or level == "Primary 2" or level == "Primary 3" or level == "Primary 4" or level == "Primary 5" or level == "Primary 6"):
             mcq_topics = mcqs[level]["topics"]
             
-            if len(topic) > 0 and topic in mcq_topics:
-                mcq_topics[topic]["question_count"] = mcq_topics[topic]["question_count"] + 1
-                mcq_topics[topic]["difficulty_1"] = mcq_topics[topic]["difficulty_1"] + 1 if difficultyLevel == "1" else mcq_topics[topic]["difficulty_1"]
-                mcq_topics[topic]["difficulty_2"] = mcq_topics[topic]["difficulty_2"] + 1 if difficultyLevel == "2" else mcq_topics[topic]["difficulty_2"]
-                mcq_topics[topic]["difficulty_3"] = mcq_topics[topic]["difficulty_3"] + 1 if difficultyLevel == "3" else mcq_topics[topic]["difficulty_3"]
-            
-            if len(subtopic) > 0 and topic in mcq_topics and subtopic in mcq_topics[topic]["subtopics"]:
-                print(topic)
-                print(subtopic)
-                mcq_topics[topic]["subtopics"][subtopic]["question_count"] = mcq_topics[topic]["subtopics"][subtopic]["question_count"] + 1
-                mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"] + 1 if difficultyLevel == "1" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"]
-                mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"] + 1 if difficultyLevel == "2" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"]
-                mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"] + 1 if difficultyLevel == "3" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"]
+            try:
+                if len(topic) > 0 and topic in mcq_topics:
+                    mcq_topics[topic]["question_count"] = mcq_topics[topic]["question_count"] + 1
+                    mcq_topics[topic]["difficulty_1"] = mcq_topics[topic]["difficulty_1"] + 1 if difficultyLevel == "1" else mcq_topics[topic]["difficulty_1"]
+                    mcq_topics[topic]["difficulty_2"] = mcq_topics[topic]["difficulty_2"] + 1 if difficultyLevel == "2" else mcq_topics[topic]["difficulty_2"]
+                    mcq_topics[topic]["difficulty_3"] = mcq_topics[topic]["difficulty_3"] + 1 if difficultyLevel == "3" else mcq_topics[topic]["difficulty_3"]
+                
+                if len(subtopic) > 0 and topic in mcq_topics and subtopic in mcq_topics[topic]["subtopics"]:
+                    print(topic)
+                    print(subtopic)
+                    mcq_topics[topic]["subtopics"][subtopic]["question_count"] = mcq_topics[topic]["subtopics"][subtopic]["question_count"] + 1
+                    mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"] + 1 if difficultyLevel == "1" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_1"]
+                    mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"] + 1 if difficultyLevel == "2" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_2"]
+                    mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"] = mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"] + 1 if difficultyLevel == "3" else mcq_topics[topic]["subtopics"][subtopic]["difficulty_3"]
+            except:
+                uuids_with_issues.append("UUID = " + uuid + " ;\tlevel = " + level + " ;\ttopic=" + topic + " ;\tsubtopic = " + subtopic + " ;\tdifficultyLevel = " + difficultyLevel)
             
 
 print(data_breakdown)
 
-# print("\nP1 Whole Numbers Easy")
-# print(p1_whole_numbers_easy_uuids)
+print(len(uuids_with_issues))
 
-# print("\nP1 Whole Numbers Normal")
-# print(p1_whole_numbers_normal_uuids)
-
+for uuid in uuids_with_issues:
+    print(uuid)
